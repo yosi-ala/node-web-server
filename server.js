41fe5a74896702f664ae2671aa318e9c73573b50
yosi-ala <yosiala1212@gmail.com>
2018-05-22 16:22:14 +0300

    Add projects page

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ app.use((req, res, next)=>{
   next();
 });
 
-app.use((req, res, next)=>{
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs');
+//   next();
+// });
 
 app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', ()=>{
@@ -51,6 +52,12 @@ app.get('/', (req, res)=>{
 app.get('/about', (req, res)=>{
   res.render('about.hbs',{
     pageTitle: 'About page'
+});
+});
+
+app.get('/projects', (req, res)=>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects page'
 });
 });
 
